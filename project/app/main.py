91a53d69aeec11765352764eff97a4bd4e13a97d
K M Jawadur Rahman <jawadur@gmail.com>
2020-09-01 20:11:40 +0600

    refactor: use APIRouter

--- project/app/main.py
+++ project/app/main.py
@@ -1,28 +1,26 @@
 import os
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.config import get_settings, Settings
+from app.api import ping
 
 
-app = FastAPI()
+def create_application() -> FastAPI:
+    application = FastAPI()
+
+    # set up Tortoise on startup and clean up on teardown
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+    application.include_router(ping.router)
+
+    return application
 
 
-# set up Tortoise on startup and clean up on teardown
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
-
-
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+app = create_application()
