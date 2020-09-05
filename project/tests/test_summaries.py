eb10286956216366bb9750bb1b1b338aad700e68
K M Jawadur Rahman <jawadur@gmail.com>
2020-09-05 12:05:21 +0600

    feat: Configure and run flake8

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -1,7 +1,5 @@
 import json
 
-import pytest
-
 
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
@@ -48,7 +46,7 @@ def test_read_all_summaries(test_app_with_db):
     response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
     summary_id = response.json()["id"]
 
-    response = test_app_with_db.get(f"/summaries")
+    response = test_app_with_db.get("/summaries")
     assert response.status_code == 200
 
     response_list = response.json()
